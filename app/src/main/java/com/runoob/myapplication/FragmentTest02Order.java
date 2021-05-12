56b0bc69ddc5dd1bcd31d7a3a8223b0f4e02203b
Andy <eerd11@bioninc.com.tw>
2021-05-12 11:55:35 +0800

    增加了數字滾輪的範例到其中一個分頁裡

--- app/src/main/java/com/runoob/myapplication/FragmentTest02Order.java
+++ app/src/main/java/com/runoob/myapplication/FragmentTest02Order.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,7 +24,9 @@ public class FragmentTest02Order extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private View view;                      //宣告一個View變數
+    private NumberPicker mNumberPicker;     //宣告一個NumberPicker變數
+    //private NumberPicker mNumberPicker2;
     public FragmentTest02Order() {
         // Required empty public constructor
     }
@@ -54,11 +57,42 @@ public class FragmentTest02Order extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+    /*@Override             //原本的onCreateView
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
 
+            return inflater.inflate(R.layout.fragment_test02_order, container, false);
+        }*/
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,  //將onCreatView改寫成這樣才能正確獲取介面元件
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test02_order, container, false);
+        view = inflater.inflate(R.layout.fragment_test02_order, container, false);
+        mNumberPicker = (NumberPicker)view.findViewById(R.id.numberPicker);     //在Fragment裡要獲取元件要這樣寫
+        mNumberPicker.setMinValue(0); //設定最小值
+        mNumberPicker.setMaxValue(200); //設定最大值
+        mNumberPicker.setValue(25);//設定現值
+        int nowValue = mNumberPicker.getValue(); //取得現值
+        mNumberPicker.setOnValueChangedListener(numPickerOnValueChange); //設定數字變化監聽事件
+        /*mNumberPicker2 = (NumberPicker)view.findViewById(R.id.numberPicker2);     //第二個數字滾輪
+        mNumberPicker2.setMinValue(0);
+        mNumberPicker2.setMaxValue(200);
+        mNumberPicker2.setValue(25);
+        int nowValue2 = mNumberPicker.getValue();
+        mNumberPicker2.setOnValueChangedListener(numPickerOnValueChange);*/
+        return view;
     }
+    private NumberPicker.OnValueChangeListener numPickerOnValueChange = new NumberPicker.OnValueChangeListener() {
+        @Override
+        public void onValueChange(NumberPicker picker, int oldVal, int newVal) {        //這裡面可規劃當數值變化要執行什麼動作
+            switch (picker.getId()){
+                case R.id.numberPicker:
+                    break;
+                /*case R.id.numberPicker2:
+                    break;*/
+            }
+
+        }
+    };
 }
--- app/src/main/java/com/runoob/myapplication/MainActivity.java
+++ app/src/main/java/com/runoob/myapplication/MainActivity.java
@@ -14,12 +14,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MainPagerAdapter adapter = new MainPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new FragmentTest01Bmi(),"BMI");
+        adapter.addFragment(new FragmentTest01Bmi(),"BMI");         //新增分頁
         adapter.addFragment(new FragmentTest02Order(),"Order");
         adapter.addFragment(new FragmentTest03Setting(),"Setting");
-        viewPager = findViewById(R.id.viewPagerMain);
-        viewPager.setAdapter(adapter);
-        viewPager.setOffscreenPageLimit(3);
+        viewPager = findViewById(R.id.viewPagerMain);   //獲取介面元件
+        viewPager.setAdapter(adapter);          //設定適配器
+        viewPager.setOffscreenPageLimit(3);     //動態載入的頁面限制
+        viewPager.setCurrentItem(1);    //可設置起始頁面由哪一頁開始，像現在就是從第二頁開始
 
     }
 }
