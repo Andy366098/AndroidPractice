1a61699c53224ac9627cd2a40b85ab896f200592
Andy <eerd11@bioninc.com.tw>
2021-05-18 18:35:17 +0800

    改成直式與橫式螢幕可使用不同布局文件(.xml)，且目前是使用橫式可上下翻轉之螢幕

--- app/src/main/java/com/runoob/myapplication/MainActivity.java
+++ app/src/main/java/com/runoob/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.runoob.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Configuration configuration = getResources().getConfiguration();
         MainPagerAdapter adapter = new MainPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new FragmentTest01Bmi(),"BMI");         //新增分頁
         adapter.addFragment(new FragmentTest02Order(),"Order");
@@ -21,6 +24,23 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);          //設定適配器
         viewPager.setOffscreenPageLimit(3);     //動態載入的頁面限制
         viewPager.setCurrentItem(1);    //可設置起始頁面由哪一頁開始，像現在就是從第二頁開始
+        /*if (savedInstanceState != null && configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){  //可設定在橫向會出現的資料
+            TextView tvSecondRow = findViewById(R.id.textView2);
+            tvSecondRow.setText("Hi,我是橫向時才會出現的喔！");
+            tvShow.setText(savedInstanceState.getString("SAVE"));
+
+        }else if (savedInstanceState != null && configuration.orientation == Configuration.ORIENTATION_PORTRAIT){
+            tvShow.setText(savedInstanceState.getString("SAVE"));
+        }*/
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {      //若有需要在螢幕翻轉時保存的資料須在此設定
+        super.onSaveInstanceState(outState);
+        /*String saveWord = tvShow.getText().toString();
+        outState.putString("SAVE",saveWord);*/
+        //mNumberPicker2.setValue(25);
 
     }
 }
